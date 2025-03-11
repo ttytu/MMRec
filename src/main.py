a1a3ee67c087828be7ee327a5ffbdd6443ea60c5
ttytu <quixote1103@gmail.com>
2025-03-11 15:27:05 +0000

    multimodal recsys incorporating review text embeddings

--- src/main.py
+++ src/main.py
@@ -10,20 +10,26 @@ Main entry
 import os
 import argparse
 from utils.quick_start import quick_start
-os.environ['NUMEXPR_MAX_THREADS'] = '48'
+
+os.environ["NUMEXPR_MAX_THREADS"] = "48"
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model', '-m', type=str, default='SELFCFED_LGN', help='name of models')
-    parser.add_argument('--dataset', '-d', type=str, default='baby', help='name of datasets')
+    parser.add_argument(
+        "--model", "-m", type=str, default="BMRE", help="name of models"
+    )
+    parser.add_argument(
+        "--dataset", "-d", type=str, default="baby", help="name of datasets"
+    )
 
     config_dict = {
-        'gpu_id': 0,
+        "gpu_id": 1,
+        "valid_metric": "Recall@20",
     }
 
     args, _ = parser.parse_known_args()
 
-    quick_start(model=args.model, dataset=args.dataset, config_dict=config_dict, save_model=True)
-
-
+    quick_start(
+        model=args.model, dataset=args.dataset, config_dict=config_dict, save_model=True
+    )
